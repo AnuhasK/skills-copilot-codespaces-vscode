e5c6cbbc387da7a4138c8757a799cc32151b527d
AnuhasK <154493387+AnuhasK@users.noreply.github.com>
2024-12-15 12:43:37 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,32 @@
+//create web server
+//create web server
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const commentsPath = path.join(__dirname, 'comments.json');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Read comments
+app.get('/comments', (req, res) => {
+    fs.readFile(commentsPath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Some error occurred');
+        }
+        res.send(data);
+    });
+});
+
+//Create comment
+app.post('/comments', (req, res) => {
+    fs.readFile(commentsPath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Some error occurred');
+        }
+        const comments = JSON.parse(data);
+        const newComment = {
+            id: comments.length + 1,
+            text: req.body.text,
